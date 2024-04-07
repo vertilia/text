7d3e970198eba233a3a7ecb68c07bc1b6c57c188
Stas Trefilov <stas.trefilov@gmail.com>
2024-04-07 23:10:34 +0200

    feat: improve comments recognition add pgettext and npgettext parsing

--- tests/xtext-test.php
+++ tests/xtext-test.php
@@ -10,28 +10,42 @@ $txt = new Text();
 /** TRANSLATORS: tagged comment of type /* */
 /// TRANSLATORS: tagged comment of type //
 ### TRANSLATORS: tagged comment of type #
-Text::_('simple $string');  // 1
+Text::_(/* TRANSLATORS: simple arg comment */ 'simple $string');  // 1
 $txt->_('simple $string');  // 1
 _('simple $string');        // 1
 gettext('simple $string');  // 1
-gettext('concat' . ' $string');  // 1
+gettext(/* TRANSLATORS: concat arg comment */ 'concat' . ' $string');  // 1
 //gettext('single form');     // 1 <- this one will generate xgettext error since clashes with plural forms
 
-Text::pget('context', 'simple message');    // 1c,2
+// TRANSLATORS: simple message comment 1
+Text::pget('context', /* TRANSLATORS: simple message comment 2 */ 'simple message');    // 1c,2
 $txt->pget('context', 'simple message');    // 1c,2
+pgettext('pgettext', 'simple message');     // 1c,2
 
-Text::nget('single form', 'plural form', 5);    // 1,2
-$txt->nget('single form', 'plural form', 5);    // 1,2
-ngettext('single form', 'plural form', 5);      // 1,2
+/* TRANSLATORS: outer comment */
+Text::nget(
+    /* TRANSLATORS: singular form comment */ 'singular form %u',
+    /* TRANSLATORS: plural comment */ 'plural form %u',
+    5
+);    // 1,2
+$txt->nget('singular form %u', 'plural form %u', 5);    // 1,2
+ngettext('singular form %u', 'plural form %u', 5);      // 1,2
 
-Text::npget('context', 'single form', 'plural form', 5);    // 1c,2,3
-$txt->npget('context', 'single form', 'plural form', 5);    // 1c,2,3
+Text::npget(
+    'context',
+    /* TRANSLATORS: singular comment */'singular form %u',
+    /* TRANSLATORS: plural comment */'plural form %u',
+    5
+);    // 1c,2,3
+$txt->npget('context', 'singular form %u', 'plural form %u', 5);    // 1c,2,3
+npgettext('context', 'singular form %u', 'plural form %u', 5);      // 1c,2,3
 
+/* TRANSLATORS: domain comment */
 dgettext('domain', 'simple message');               // 2
 dcgettext('domain', 'simple message', LC_MESSAGES); // 2
 
-dngettext('domain', 'single form', 'plural form');                  // 2,3
-dcngettext('domain', 'single form', 'plural form', 5, LC_MESSAGES); // 2,3
+dngettext('domain', 'singular form %u', 'plural form %u');                  // 2,3
+dcngettext('domain', 'singular form %u', 'plural form %u', 5, LC_MESSAGES); // 2,3
 
 $txt->_("complex \n\"\$string\"");
 
